5dd3fb3fcc232e870a6baa3801d46dc238fbb389
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-01-25 23:55:24 +0100

    add game board and logic to detect captured boxes

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,71 @@
 import { type NextPage } from "next";
+import { Fragment, useState } from "react";
 
 const Game: NextPage = () => {
-    return (
-        <main className="flex justify-center items-center h-[100vh] flex-col">
-            <div>
-                game board
+  const [clicked, setClicked] = useState<number[]>([]);
+
+  const handleClick = (id: number) => {
+    setClicked((x) => [...x, id]);
+  };
+
+  const isOwned = (row: number, col: number) => {
+    const id = parseInt(`${row}${col}`);
+
+    const neededIds = [
+      id,
+      id + 1,
+      parseInt(`${row - 1}${col}`),
+      parseInt(`${row + 1}${col}`),
+    ];
+
+    return neededIds.every((elem) => clicked.includes(elem));
+  };
+
+  return (
+    <main className="flex h-[100vh] flex-col items-center justify-center gap-4">
+      <div>
+        <p className="mb-6 text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+          Your Turn
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((row) => {
+          const isBigRow = !(row % 2);
+          return (
+            <div
+              key={row}
+              className={`flex flex-row ${isBigRow ? "" : "gap-6"}`}
+            >
+              {[1, 2, 3, 4, 5, 6]
+                .filter((x) => isBigRow || x !== 6)
+                .map((col) => {
+                  const id = parseInt(`${row}${col}`);
+                  return (
+                    <Fragment key={id}>
+                      <div
+                        onClick={() => handleClick(id)}
+                        data-id={id}
+                        className={`cursor-pointer rounded bg-gray-900 ${
+                          isBigRow ? "h-[100px] w-6" : "h-6 w-[100px]"
+                        }`}
+                      ></div>
+                      {isBigRow && col !== 6 && (
+                        <div
+                          data-id={id}
+                          className={`w-[100px] ${
+                            isOwned(row, col) ? "bg-blue-500" : ""
+                          }`}
+                        ></div>
+                      )}
+                    </Fragment>
+                  );
+                })}
             </div>
-        </main>
-    );
+          );
+        })}
+      </div>
+    </main>
+  );
 };
 
-export default Game
+export default Game;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,28 @@ import Head from "next/head";
 import { api } from "../utils/api";
 import LandingScreen from "@components/LandingScreen";
 import RoomList from "@components/RoomList";
-import {useState} from "react";
+import { useState } from "react";
 import Room from "@components/Room";
 import Game from "@components/Game";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const [page, setPage] = useState('')
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const [page, setPage] = useState("");
 
   return (
     <>
       <Head>
         <title>Dots & Boxes</title>
-        <meta name="description" content="Dots and Boxes classic pencil and paper online game to play in your browser.-app" />
+        <meta
+          name="description"
+          content="Dots and Boxes classic pencil and paper online game to play in your browser.-app"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        {page === '' && <LandingScreen setPage={setPage} />}
-        {page === 'room-list' && <RoomList />}
-        {page === 'room' && <Room setPage={setPage} />}
-        {page === 'game' && <Game />}
+      {page === "" && <LandingScreen setPage={setPage} />}
+      {page === "room-list" && <RoomList />}
+      {page === "room" && <Room setPage={setPage} />}
+      {page === "game" && <Game />}
     </>
   );
 };
