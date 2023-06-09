b6bade9d5a42fe84c39606729692aa86a3ed79fe
MarconLP <13001502+MarconLP@users.noreply.github.com>
2023-06-09 12:11:04 +0300

    remove unnecessary env variables

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,19 +8,6 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET:
-    process.env.NODE_ENV === "production"
-      ? z.string().min(1)
-      : z.string().min(1).optional(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
   PUSHER_APP_ID: z.string(),
   PUSHER_APP_KEY: z.string(),
   PUSHER_APP_SECRET: z.string(),
@@ -35,10 +22,6 @@ export const serverSchema = z.object({
 export const serverEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   PUSHER_APP_ID: process.env.PUSHER_APP_ID,
   PUSHER_APP_KEY: process.env.PUSHER_APP_KEY,
   PUSHER_APP_SECRET: process.env.PUSHER_APP_SECRET,
@@ -66,5 +49,4 @@ export const clientEnv = {
   NEXT_PUBLIC_PUSHER_APP_KEY: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
   NEXT_PUBLIC_PUSHER_APP_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
   NEXT_PUBLIC_POSTHOG_TOKEN: process.env.NEXT_PUBLIC_POSTHOG_TOKEN
-
 };
